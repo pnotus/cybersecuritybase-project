3eeb1a80bab9caace88f5051f75df697abb8399b
Pontus Frendin <pontus@frend.in>
2017-12-20 21:18:17 +0100

    Added xss vulnerability on the sign up page

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -2,6 +2,7 @@ package sec.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,8 +26,9 @@ public class SignupController {
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String submitForm(@RequestParam String name, @RequestParam String address) {
+    public String submitForm(Model model, @RequestParam String name, @RequestParam String address) {
         signupRepository.save(new Signup(name, address));
+        model.addAttribute("name", name);
         return "done";
     }
 
