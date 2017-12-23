f07e3cfd23caeecdaa9c494028900c5244ee20c0
Pontus Frendin <pontus@frend.in>
2017-12-23 19:41:33 +0100

    Added Insecure Direct Object References vulnerability

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -1,8 +1,16 @@
 package sec.project.controller;
 
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,6 +38,28 @@ public class SignupController {
         signupRepository.save(new Signup(name, address));
         model.addAttribute("name", name);
         return "done";
+    }
+    
+    @RequestMapping(value = "/files/{fileName:.+}", method = RequestMethod.GET)
+    public ResponseEntity<InputStreamResource> getFile(@PathVariable("fileName") String fileName) throws IOException {
+        ClassPathResource file = new ClassPathResource(fileName);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("text/plain"));
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT");
+        headers.add("Access-Control-Allow-Headers", "Content-Type");
+        headers.add("Content-Disposition", "filename=" + fileName);
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        headers.setContentLength(file.contentLength());
+
+        ResponseEntity<InputStreamResource> response = 
+                new ResponseEntity<InputStreamResource>(new InputStreamResource(file.getInputStream()), headers, HttpStatus.OK);
+        
+        return response;
     }
 
 }
